0e24637dd5b512b9b631999e6df5c127f916bc9c
petercrippsIBM <pete_cripps@uk.ibm.com>
2018-08-09 22:43:38 +0100

    Refactored frontend JS

--- art-ledger-app/views/script.js
+++ art-ledger-app/views/script.js
@@ -1,194 +1,200 @@
+/* This is the API of the REST server used for making HTTP requests against. */
+const RESTAPI = "https://art-ledger.mybluemix.net/api/";
+
+/* This is the namespace used for the organisation. */
+const NAMESPACE = "org.artledger";
+
+/* Set this to 'true' to write to the console rather than read/write the ledger. */
+const DEBUGMODE = false;
+
 /* getParticipantsInfo()
  * Get the information on all participants of type 'pType' in the ledger.
+ * 'elementId' is the where the callback function writes to on return.
 */
-function getParticipantsInfo(pType, cFunction) {
-  var xhttp;
-  url = "https://art-ledger.mybluemix.net/api/"+pType;
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "participantsInfo");
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
+function getParticipantsInfo(pType, elementId) {
+  httpGET(RESTAPI+pType, elementId);
 }
 
 /* getParticipantInfo()
  * Get the information on a single particpant of type 'pType' in the ledger.
+ * 'elementId' is the where the callback function writes to on return.
+ * 'formId' is the form where the input data is obtained from.
 */
-function getParticipantInfo(pType, cFunction) {
-  var xhttp;
-  var url = "https://art-ledger.mybluemix.net/api/"+pType+"/";
-  var id = document.forms["getParticipant"]["participantId"].value;
+function getParticipantInfo(pType, elementId, formId) {
+  var id = document.forms[formId]["participantId"].value;
 
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "participantInfo");
-    }
-  }
-  xhttp.open("GET", url+id, true);
-  xhttp.send();
+  httpGET(RESTAPI+pType+"/"+id, elementId);
 }
 
 /* createParticipantInfo()
  * Create the information for a new participant of type 'pType' in the ledger.
+ * 'elementId' is the where the callback function writes to on return.
+ * 'formId' is the form where the input data is obtained from.
  * Actual information will vary depending on the 'pType'.
 */
-function createParticipantInfo(pType, cFunction) {
-  var xhttp;
-  var url = "https://art-ledger.mybluemix.net/api/"+pType;
-  var input = document.forms["newParticipant"];
-  var obj;
+function createParticipantInfo(pType, elementId, formId) {
+  var input = document.forms[formId];
   var inputJSON;
+  var obj;
 
   switch(pType) {
-      case "Artist":
-        obj = {
-          $class: "org.artledger.Artist",
-          artistId: input["artistId"].value,
-          firstName: input["firstName"].value,
-          lastName:input["lastName"].value};
-          break;
-      case "Owner":
-        var dealer = false;
-        if (input["dealer"].value == "yes") {
-            dealer = true;
-        }
-        obj = {
-          $class: "org.artledger.Owner",
-          ownerId: input["ownerId"].value,
-          firstName: input["firstName"].value,
-          lastName: input["lastName"].value,
-          isDealer: dealer};
-          break;
-      default:
-          console.log("no such participant type");
+    case "Artist":
+      obj = {
+        $class: NAMESPACE+"."+pType,
+        artistId: input["artistId"].value,
+        firstName: input["firstName"].value,
+        lastName:input["lastName"].value};
+        break;
+    case "Owner":
+      var dealer = false;
+      if (input["dealer"].value == "yes") {
+        dealer = true;
+      }
+      obj = {
+        $class: NAMESPACE+"."+pType,
+        ownerId: input["ownerId"].value,
+        firstName: input["firstName"].value,
+        lastName: input["lastName"].value,
+        isDealer: dealer};
+        break;
+    default:
+      console.log("no such participant type");
   }
-  inputJSON = JSON.stringify(obj);
-  // console.log(inputJSON);
-
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "newParticipantInfo");
-    }
-  }
-
-  xhttp.open("POST", url, true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(inputJSON);
-
+  httpPOST(RESTAPI+pType, elementId, JSON.stringify(obj));
 }
 
 /* getAssetsInfo()
- * Get the information on all assets of type 'aType' in the ledger.
+* Get the information on all assets of type 'aType' in the ledger.
+* 'elementId' is the where the callback function writes to on return.
 */
-function getAssetsInfo(aType, cFunction) {
-  var xhttp;
-  url = "https://art-ledger.mybluemix.net/api/"+aType;
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "assetsInfo");
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
+function getAssetsInfo(aType, elementId) {
+  httpGET(RESTAPI+aType, elementId);
 }
 
 /* getAssetInfo()
- * Get the information on a single asset of type 'aType' in the ledger.
+* Get the information on a single asset of type 'aType' in the ledger.
+* 'elementId' is the where the callback function writes to on return.
+* 'formId' is the form where the input data is obtained from.
 */
-function getAssetInfo(aType, cFunction) {
-  var xhttp;
-  var url = "https://art-ledger.mybluemix.net/api/"+aType+"/";
-  var id = document.forms["getAsset"]["assetId"].value;
+function getAssetInfo(aType, elementId, formId) {
+  var id = document.forms[formId]["assetId"].value;
 
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "assetInfo");
-    }
-  }
-  xhttp.open("GET", url+id, true);
-  xhttp.send();
+  httpGET(RESTAPI+aType+"/"+id, elementId);
 }
 
 /* createAssetInfo()
- * Create the information for a new asset of type 'aType' in the ledger.
- * Actual information will vary depending on the 'aType'.
+* Create the information for a new asset of type 'aType' in the ledger.
+* 'elementId' is the where the callback function writes to on return.
+* 'formId' is the form where the input data is obtained from.
+* Actual information will vary depending on the 'aType'.
 */
-function createAssetInfo(aType, cFunction) {
-  var xhttp;
-  var url = "https://art-ledger.mybluemix.net/api/"+aType;
-  var input = document.forms["newAsset"];
+function createAssetInfo(aType, elementId, formId) {
+  var input = document.forms[formId];
   var obj;
-  var inputJSON;
   var auction;
-  var thisArtist="resource:org.artledger.Artist#"+input["artist"].value;
-  var thisOwner="resource:org.artledger.Owner#"+input["owner"].value;
+  var thisArtist="resource:"+NAMESPACE+".Artist#"+input["artist"].value;
+  var thisOwner="resource:"+NAMESPACE+".Owner#"+input["owner"].value;
+  var url = RESTAPI+aType;
 
-  auction = false;
-  if (input["underAuction"].value == "yes") {
+  if (aType == "ArtWork") {
+    auction = false;
+    if (input["underAuction"].value == "yes") {
       auction = true;
-  }
-
-  obj = {
-    $class: "org.artledger.ArtWork",
-    artId: input["artId"].value,
-    type: input["artType"].value,
-    description: input["description"].value,
-    currentValue: Number(input["currentValue"].value),
-    underAuction: auction,
-    artist: thisArtist,
-    owner: thisOwner};
-
-  inputJSON = JSON.stringify(obj);
-  // console.log(inputJSON);
-
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "newAssetInfo");
     }
-  }
 
-  xhttp.open("POST", url, true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(inputJSON);
+    obj = {
+      $class: "org.artledger.ArtWork",
+      artId: input["artId"].value,
+      type: input["artType"].value,
+      description: input["description"].value,
+      currentValue: Number(input["currentValue"].value),
+      underAuction: auction,
+      artist: thisArtist,
+      owner: thisOwner};
+    httpPOST(RESTAPI+aType, elementId, JSON.stringify(obj));
+  }
 }
 
 /* transactAsset()
  * Excute a transaction against an asset. The transaction is of type 'tType'.
+ * 'elementId' is the where the callback function writes to on return.
+ * 'formId' is the form where the input data is obtained from.
+ * Actual information will vary depending on the 'aType'.
 */
-function transactAsset(tType, cFunction) {
-  var xhttp;
-  var url = "https://art-ledger.mybluemix.net/api/"+tType;
-  var input = document.forms["sellAsset"];
+function transactAsset(tType, elementId, formId) {
+  var input = document.forms[formId];
   var obj;
-  var inputJSON;
 
-  obj = {
-    $class: "org.artledger.SellArtWork",
-    newValue: Number(input["sellAmount"].value),
-    artWork: "resource:org.artledger.ArtWork#"+input["artId"].value,
-    newOwner: "resource:org.artledger.Owner#"+input["newOwner"].value,
-    transactionId: ""};
-
-  inputJSON = JSON.stringify(obj);
-  console.log(inputJSON);
-
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this, "sellAssetInfo");
-    }
+  switch(tType) {
+    case "SellArtWork":
+      obj = {
+        $class: NAMESPACE+"."+tType,
+        newValue: Number(input["sellAmount"].value),
+        artWork: "resource:org.artledger.ArtWork#"+input["artId"].value,
+        newOwner: "resource:org.artledger.Owner#"+input["newOwner"].value,
+        transactionId: ""};
+      break;
+    case "AuctionArtWork":
+      obj = {
+        $class: NAMESPACE+"."+tType,
+        artWork: "resource:org.artledger.ArtWork#"+input["artId"].value,
+        transactionId: ""};
+      break;
+    case "BidOnArtWork":
+      obj = {
+        $class: NAMESPACE+"."+tType,
+        bidAmount: Number(input["bidAmount"].value),
+        artWork: "resource:org.artledger.ArtWork#"+input["artId"].value,
+        transactionId: ""};
+      break;
+      default:
+        console.log("no such transaction type");
   }
-  xhttp.open("POST", url, true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(inputJSON);
+  httpPOST(RESTAPI+tType, elementId, JSON.stringify(obj));
+}
+
+/* httpGET
+ * Make an asynchrous GET request to the specified 'uri'.
+ * Async response is sent to a callback function and
+ * the response text written to 'elementId'.
+ */
+function httpGET(uri, elementId) {
+   var xhttp=new XMLHttpRequest();
+
+   if (DEBUGMODE) {
+     console.log("GET: ",uri);
+   } else {
+     xhttp.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
+         callback(this, elementId);
+       }
+     };
+     xhttp.open("GET", uri, true);
+     xhttp.send();
+   }
+}
+
+/* httpPOST
+ * Make an asynchrous POST request to the specified 'uri'.
+ * inputJSON is the data to send in the POST.
+ * Async response is sent to a callback function and
+ * the response text written to 'elementId'.
+ */
+function httpPOST(uri, elementId, inputJSON) {
+   var xhttp=new XMLHttpRequest();
+
+   if (DEBUGMODE) {
+     console.log("POST: ",uri, "Data: ", inputJSON);
+   } else {
+     xhttp.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
+         callback(this, elementId);
+       }
+     };
+     xhttp.open("POST", uri, true);
+     xhttp.setRequestHeader("Content-type", "application/json");
+     xhttp.send(inputJSON);
+  }
 }
 
 /* callback()
